d1a8ff3edb6f78eb9f81a3724c0263bd313918fa
Jignesh Modi <jigneshmodi.developer@gmail.com>
2020-02-01 13:09:23 -0800

    Game Activity

---
+++ app/src/main/java/com/a/bini/GameActivity.java
@@ -0,0 +1,14 @@
+package com.a.bini;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
--- app/src/main/java/com/a/bini/MainActivity.java
+++ app/src/main/java/com/a/bini/MainActivity.java
@@ -2,6 +2,7 @@ package com.a.bini;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,30 +10,44 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button nextActivityBtn;
+    private Button snapActivityBtn;
+    private Button gameActivityBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        nextActivityBtn.setOnClickListener(new View.OnClickListener() {
+        snapActivityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToNextActivity();
+                goToSnapActivity();
+            }
+        });
+
+        gameActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToGameActivity();
             }
         });
     }
 
     private void init() {
-        nextActivityBtn = findViewById(R.id.nextActivityBtn);
+        snapActivityBtn = findViewById(R.id.snapActivityBtn);
+        gameActivityBtn = findViewById(R.id.gameActivityBtn);
     }
 
-    private void goToNextActivity() {
+    private void goToSnapActivity() {
         Intent intent = new Intent(this, SnapActivity.class);
 //        EditText editText = (EditText) findViewById(R.id.editText);
 //        String message = editText.getText().toString();
 //        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    private void goToGameActivity() {
+        Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
 }
